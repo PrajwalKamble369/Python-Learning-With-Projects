8180524094e75c0c4a60228ad43f14ebcc5226e3
Prajwal <prajwal17022000@gmail.com>
2026-01-02 21:15:15 +0530

    Module accesing and creation

---
+++ learning/.ipynb_checkpoints/mymodule-checkpoint.py
@@ -0,0 +1,10 @@
+# mymodule.py
+def greeting(name):
+    """Prints a personalized greeting."""
+    print(f"Hello, {name}!")
+
+person1 = {
+    "name": "John",
+    "age": 36,
+    "country": "Norway"
+}
